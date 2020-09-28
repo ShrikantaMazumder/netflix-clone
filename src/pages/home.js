fff1cbc4d356edf00fa55ce652716173865f6e65
ShrikantaMazumder <shrikantamazumder@gmail.com>
2020-09-28 09:05:53 +0600

    header component p-1

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import HeaderContainer from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqContainer } from '../containers/faqs';
 import FooterContainer from '../containers/footer';
+
 
 
 const Home = () => {
     
     return (
         <>
-            <JumbotronContainer />
-            <FaqContainer />
-            <FooterContainer />
+            <HeaderContainer>
+                <JumbotronContainer />
+                <FaqContainer />
+                <FooterContainer />
+            </HeaderContainer>
         </>
     );
 };
